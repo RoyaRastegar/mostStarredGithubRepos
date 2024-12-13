e1dba49842aa91052bf7f6c78506b61a3509e87a
Roya Rastegar <roya.rastegar54196>
2024-12-13 17:23:54 +0800

    update app component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/home/Home';
+function App() {
+  return (
+    <div className='h-screen flex items-center justify-center bg-gray-100 '>
+      <div className='w-full sm:w-[30%] h-full sm:h-[90%]'>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
